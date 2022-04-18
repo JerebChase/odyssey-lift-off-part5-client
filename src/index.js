a9c6a1e3accb84f4a29d121843ccb4130aaf79cb
Jeremy Brown <chase208@4JLT.com>
2022-04-18 15:38:35 -0400

    trying something else

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://jb-server-catstronauts.herokuapp.com/',
+  uri: 'https://jb-server-catstronauts.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
